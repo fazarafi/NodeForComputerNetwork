50085774722d18f73a381b02efc1de18071e33e7
cmrudi <13514057@std.stei.itb.ac.id>
2016-10-05 10:50:12 +0700

    u

--- HelloServer.c
+++ HelloServer.c
@@ -5,14 +5,17 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <string.h>
 
-main()
+int main()
 {
 	int sock,cli;
 	struct sockaddr_in server, client;
 	unsigned int len;
-	char mesg[] = "Hello to the world of socket programming";	
+	char mesg[] = "Hello";	
 	int sent;
+	int data_len;
+	char data[1024];
 
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{
@@ -46,7 +49,23 @@ main()
 			perror("accept");
 			exit(-1);
 		}
-		sent = send(cli, mesg, strlen(mesg), 0);
+			sent = send(cli, mesg, strlen(mesg), 0);
+			
+		data_len = 1;
+		while(data_len) {
+			data_len = recv(cli, data, 1024,0);
+			if (data_len) {
+				send(cli, data, data_len-1,0);
+				data[data_len] = '\0';
+				printf(data);
+			}
+				printf(data_len);
+			}
+			if (!strcmp(data,mesg)) {
+				printf("My name is rudi \n");
+			}
+			
+		}
 		
 
 		printf("send %d bytes to client : %s\n", sent, inet_ntoa(client.sin_addr));
@@ -55,5 +74,6 @@ main()
 
 	}
 	close(cli);
+	return 0;
 }
 
